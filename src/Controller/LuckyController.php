f8b1b116813dd79a3c68456632137462facb9507
stratosphr <stratosphr@gmail.com>
2019-08-03 22:38:08 +0200

    Added profiler

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,10 +3,11 @@
 	namespace App\Controller;
 	
 	use Exception;
+	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
 	
-	class LuckyController {
+	class LuckyController extends AbstractController {
 		
 		/**
 		 * @Route("lucky/number")
